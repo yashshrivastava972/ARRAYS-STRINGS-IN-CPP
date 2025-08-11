50ab84125c818ceefa6ef764be6344569cbb776e
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-11 23:13:47 +0530

    Create PROGRAM 7H.cpp

---
+++ PROGRAM 7H.cpp
@@ -0,0 +1,22 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: CONCATENATION OF STRINGS (EXPERIMENT: 7H)
+
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   string str;
+   cout<<"Enter a String: ";
+   cin>>str;
+   cout<<str<<endl;
+}
+
+// OUTPUT:
+
+// Enter a String: YSHRIVASTAVA
+// YSHRIVASTAVA
