8c797a8c2376317a108e29b8f6989693ae65f67d
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-11 22:59:58 +0530

    Create PROGRAM 7A.cpp

---
+++ PROGRAM 7A.cpp
@@ -0,0 +1,33 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: ARRAYS IN C++ (EXPERIMENT: 7A)
+
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+int main() {
+   int numbers[5];
+   
+   cout<<"ENTER 5 NUMBERS:";
+   
+   for(int i=0; i<5;i++) {
+       cin>>numbers[i];
+   } 
+
+   for (int n=0; n<5; n++) {
+          cout<<numbers[n]<<" ";
+   }
+    return 0;
+} 
+
+// OUTPUT:
+
+// ENTER 5 NUMBERS:10
+// 20
+// 30
+// 40
+// 50
+// 10 20 30 40 50 
