bd5ddf60653fabbf676305474046a3b25a258b6e
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-11 23:06:11 +0530

    Update and rename PROGRAM 6C.cpp to PROGRAM 7C.cpp

---
+++ PROGRAM 7C.cpp
@@ -0,0 +1,46 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: REVERSE OF AN ARRAY (EXPERIMENT: 7C)
+
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+// OUTPUT:
+
+// Enter number of elements: 5
+// Enter elements: 10
+// 20
+// 30
+// 40
+// 50
+// Reversed array: 50 40 30 20 10 
