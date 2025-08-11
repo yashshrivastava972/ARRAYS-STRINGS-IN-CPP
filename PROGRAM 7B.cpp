5f88e8009aaf712186adf6e9144e32f8c2744cc4
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-11 23:00:42 +0530

    Create PROGRAM 7B.cpp

---
+++ PROGRAM 7B.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5];
+    int key, i;
+
+    cout << "ENTER 5 NUMBERS: ";
+    for (i = 0; i < 5; i++) {
+        cin >> marks[i];
+    }
+
+    cout << "ENTER THE NUMBER TO SEARCH: ";
+    cin >> key;
+
+    for (i = 0; i < 5; i++)
+        if (key == marks[i]) {
+            cout << "KEY FOUND AT LOCATION: " << i;
+            break;
+        }
+
+    return 0;
+}
+
+// OUTPUT:
+
+// ENTER 5 NUMBERS: 90
+// 92
+// 94
+// 93
+// 95
+// ENTER THE NUMBER TO SEARCH: 95
+// 				KEY FOUND AT LOCATION: 4
