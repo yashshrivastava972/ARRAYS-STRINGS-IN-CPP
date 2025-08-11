973022ab96809edcbd767644968dc183839dc86c
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-11 23:12:04 +0530

    Create PROGRAM 7F.cpp

---
+++ PROGRAM 7F.cpp
@@ -0,0 +1,26 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: DISPLAYING A STRING (EXPERIMENT: 7F)
+
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   string str1,str2,str3;
+   cout<<"Enter a String: ";
+   cin>>str1;
+   cout<<"Enter a String for concatenation : ";
+   cin>>str2;
+   str3=str1 + " " + str2; 
+   cout<<str3;
+}
+
+// OUTPUT:
+
+// Enter a String: YASH
+// Enter a String for concatenation : SHRIVASTAVA
+// YASH SHRIVASTAVA
